f5bd73cb04e7690b37f94fb256a0a54c1ecb12fe
Deakon89 <franck.devw@gmail.com>
2025-04-03 14:55:30 +0200

    Update auth-service.service.ts

--- urban-tribe/src/app/service/auth-service.service.ts
+++ urban-tribe/src/app/service/auth-service.service.ts
@@ -1,30 +1,7 @@
-// import { Injectable } from '@angular/core';
-
-// @Injectable({ providedIn: 'root' })
-// export class AuthService {
-//   private validToken = 'c';  // Questo dovrebbe essere il token che aspetti.
-//   private isLoggedIn = false;
-
-//   validateToken(token: string): boolean {
-//     return token === this.validToken;
-//   }
-
-//   login(token: string): boolean {
-//     // Qui dovresti validare il token e impostare isLoggedIn a true se è valido
-//     this.isLoggedIn = token === this.validToken; // Sostituisci con la tua logica di validazione
-//     return this.isLoggedIn;
-//   }
-//     logout(): void {
-//       this.isLoggedIn = false;
-//     }
-  
-//     isAuthenticated(): boolean {
-//       return this.isLoggedIn;
-//     }
-//   }
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { CookieService } from 'ngx-cookie-service'; // Assicurati di installare ngx-cookie-service
+import { CookieService } from 'ngx-cookie-service';
+import { environment } from '../../enviroment/enviroment';
 
 
 @Injectable({
@@ -34,7 +11,6 @@ export class AuthService {
   constructor(private http: HttpClient, private cookieService: CookieService) {}
   
   login(token: string): boolean {
-    // Qui puoi validare il token con il server
     if (this.validateToken(token)) {
       this.cookieService.set('auth_token', token, 1, '/', undefined, true, 'Strict');
       return true;
@@ -51,7 +27,7 @@ export class AuthService {
     return !!token && this.validateToken(token);
   }
 
-  public readonly validToken = 'cacchio';
+  public readonly validToken = environment.token;
   public validateToken(token: string): boolean {
     return token === this.validToken;
   }
